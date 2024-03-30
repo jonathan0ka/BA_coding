2acfe28969f022081f7a3e5fc3fb6b60b13a9491
jonathan0ka <146357839+jonathan0ka@users.noreply.github.com>
2024-03-30 20:43:18 +0100

    gksjehg

--- python_scripts/refinitiv_eikon_requests/test_request_v2.py
+++ python_scripts/refinitiv_eikon_requests/test_request_v2.py
@@ -5,13 +5,13 @@ import datetime
 
 ek.set_app_key('4b3a2041ad65478b91d46404ba35a4f4d2413f6c')
 
-sdate_for_year = "2006-01-01"
+sdate_for_year = "2018-01-01" #licence from UZH: 2017-01-01
 
     # Get the constituents of the index
-    df, err = ek.get_data(
-        instruments=['.STOXX'],
-        fields=['TR.IndexConstituentRIC', 'TR.IndexConstituentName'],
-        parameters={'SDate': sdate_for_year}
+df, err = ek.get_data(
+    instruments=['.SSMI'],
+    fields=['TR.IndexConstituentRIC', 'TR.IndexConstituentName'],
+    parameters={'SDate': sdate_for_year}
     )
 
 print(df)
