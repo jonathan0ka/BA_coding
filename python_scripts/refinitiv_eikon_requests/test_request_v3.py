09cdeecaf1838371608e1dcc725317dd983c2a60
Jonathan Alexander Zeh <jonathanalexander.zeh@uzh.ch>
2024-03-31 13:38:06 +0200

    rewr

--- python_scripts/refinitiv_eikon_requests/test_request_v3.py
+++ python_scripts/refinitiv_eikon_requests/test_request_v3.py
@@ -15,35 +15,31 @@ ek.set_app_key('4b3a2041ad65478b91d46404ba35a4f4d2413f6c')
 
 ########################## ticker without stock exchange code
 # Specify just the ticker symbol without the exchange code
-ticker_symbol = 'ABBN.S'  # Apple's ticker symbol, without specifying the exchange
+# ticker_symbol = 'ABBN.S'  # Apple's ticker symbol, without specifying the exchange
 
-# Define the fields you want to retrieve
-fields = ['TR.CompanyName', 'TR.PriceClose', 'TR.Volume']
+# # Define the fields you want to retrieve
+# fields = ['TR.CompanyName', 'TR.PriceClose', 'TR.Volume']
 
-try:
-    # Fetch the data
-    data, err = ek.get_data(instruments=[ticker_symbol], fields=fields)
+# try:
+#     # Fetch the data
+#     data, err = ek.get_data(instruments=[ticker_symbol], fields=fields)
 
-    if err:
-        print(f"Error: {err}")
-    else:
-        print("Data retrieved successfully:")
-        print(data)
-except Exception as e:
-    print(f"An error occurred: {e}")
+#     if err:
+#         print(f"Error: {err}")
+#     else:
+#         print("Data retrieved successfully:")
+#         print(data)
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 
 #######################
-x='aapl'
-x=x.upper()
-test = ek.json_requests.send_json_request('SymbologySearch',{'symbols': [x], 'from': 'ticker', 'to': ['RIC'], 'bestMatchOnly': False})
-symbol=list()
+fields = ["TR.OrganizationID"]
 
-try:
-for key,value in test.items():
-value = value[0]
-for key1,value1 in value.items():
-symbol=value1[0]
-except:
-pass
-print( symbol)
+'''where "search" would be the name of a company'''
+
+exp = "SCREEN(U(IN(Equity(active,public,private,primary))), Contains(TR.CommonName,%s), CURN=CAD)" %"ABB"
+
+
+df, e = ek.get_data(exp, fields)
+print(df)
