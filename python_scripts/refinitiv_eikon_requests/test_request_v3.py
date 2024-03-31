b225573f3391af17c335be3cc4f4b70b7a602803
jonathan0ka <146357839+jonathan0ka@users.noreply.github.com>
2024-03-31 13:25:56 +0200

    sf

--- python_scripts/refinitiv_eikon_requests/test_request_v3.py
+++ python_scripts/refinitiv_eikon_requests/test_request_v3.py
@@ -15,7 +15,7 @@ ek.set_app_key('4b3a2041ad65478b91d46404ba35a4f4d2413f6c')
 
 ########################## ticker without stock exchange code
 # Specify just the ticker symbol without the exchange code
-ticker_symbol = 'ABBN'  # Apple's ticker symbol, without specifying the exchange
+ticker_symbol = 'ABBN.S'  # Apple's ticker symbol, without specifying the exchange
 
 # Define the fields you want to retrieve
 fields = ['TR.CompanyName', 'TR.PriceClose', 'TR.Volume']
