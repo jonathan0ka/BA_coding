4f2b4be66e5c24fe3c24918c9356282389ffeee4
Jonathan Alexander Zeh <jonathanalexander.zeh@uzh.ch>
2024-03-31 11:56:59 +0200

    sf

---
+++ python_scripts/refinitiv_eikon_requests/test_request_v3.py
@@ -0,0 +1,13 @@
+import eikon as ek
+
+# Set your Eikon API key
+ek.set_app_key('4b3a2041ad65478b91d46404ba35a4f4d2413f6c')
+
+# Use a company name to search for its ticker
+company_name = "Google"  # Example company name
+data, err = ek.get_data(instruments=[f"R:{company_name}"], fields=['RIC', 'Name', 'ExchangeName', 'CountryISO'])
+
+if err is None:
+    print(data)
+else:
+    print(f"Error: {err}")
