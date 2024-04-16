2d2509de779f94c426b889ef3e1a58d4cf796c74
Jonathan Alexander Zeh <jonathanalexander.zeh@uzh.ch>
2024-04-16 16:04:09 +0200

    uh

--- python_scripts/refinitiv_eikon_requests/stock_level/get_stock_data.py
+++ python_scripts/refinitiv_eikon_requests/stock_level/get_stock_data.py
@@ -7,30 +7,39 @@ from datetime import datetime
 ek.set_app_key('4b3a2041ad65478b91d46404ba35a4f4d2413f6c')
 
 # Define the date range
-start_date = '2023-01-01'
+start_date = '2006-01-01'
 end_date = '2024-01-01'
 
 # Import the CSV file containing the stock RICs
-ric_df = pd.read_csv('C:\\Users\\Shadow\\OneDrive\\BA_Thesis\\BA_coding\\datasets\\eikon_data\\constituents_stoxx_europe_600.csv')  # Make sure to provide the correct path
+ric_df = pd.read_csv('C:\\Users\\Shadow\\OneDrive\\BA_Thesis\\BA_coding\\datasets\\eikon_data\\index_constituents_data\\constituents_stoxx_europe_600.csv')
 
-# Extract the RICs into a list
-ric_list = ric_df['Constituent RIC'].tolist()
+# get unqiue stock_RIC
+ric_list = unique(ric_df['Constituent RIC'].tolist())
 print(ric_list)
 
 # Initialize an empty DataFrame to aggregate the results
 aggregated_df = pd.DataFrame()
 
 fields = ["TR.PriceClose.date",
-          "TR.PriceClose(Scale=0)", 
-          "TR.TotalReturn1D", 
-          "TR.Volume(Scale = 0)", 
-          "TR.TURNOVER", 
-          "TR.CompanyMarketCap", 
+          "TR.PriceClose(Scale=0)",
+          ##########################
+          "TR.TotalReturn1D",
+          ##########################
+          "TR.Volume(Scale = 0)",
+          #"TR.Volume.date"
+          ##########################
+          "TR.TURNOVER",
+          #"TR.TURNOVER.date",
+          ##########################
+          "TR.CompanyMarketCap",
+          #"TR.CompanyMarketCap.date",
+          ##########################
           "TR.GrossProfit(Period=FY0)", 
           "TR.PriceToBVPerShare", #book value / price
           "TR.BIDPRICE", #last bidprice of previous day
           "TR.ASKPRICE", #last askprice of previous day
-          "TR.MIDPRICE"] #last midprice of previous day #returns NAs only
+          #"TR.ASKPRICE.date"
+          ]
 
 aggregated_df, e = ek.get_data(instruments = ric_list,
                     fields = fields,
@@ -44,6 +53,6 @@ print(type(aggregated_df))
 # Convert to datetime
 aggregated_df['Date'] = pd.to_datetime(df['Date'])
 
-file_path = "C:\\Users\\Shadow\\OneDrive\\BA_Thesis\\BA_coding\\datasets\\eikon_data\\stock_level_data.csv"
+file_path = "C:\\Users\\Shadow\\OneDrive\\BA_Thesis\\BA_coding\\datasets\\eikon_data\\stock_level_data\\stock_level_data.csv"
 aggregated_df.to_csv(file_path, index=False)
 print(f"Data exported successfully")
