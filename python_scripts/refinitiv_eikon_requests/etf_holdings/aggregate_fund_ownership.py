307d494f8f3988e7219b761270a5b1a2c31f12ea
Jonathan Alexander Zeh <jonathanalexander.zeh@uzh.ch>
2024-04-29 12:39:03 +0200

    ij

--- python_scripts/refinitiv_eikon_requests/etf_holdings/aggregate_fund_ownership.py
+++ python_scripts/refinitiv_eikon_requests/etf_holdings/aggregate_fund_ownership.py
@@ -7,5 +7,5 @@ df = pd.read_csv('/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_T
 grouped_df = df.groupby(['stock_RIC', 'date'])['market_cap_fund'].sum().reset_index()
 
 # Export to new CSV
-file_path = "/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_Thesis/BA_coding/datasets/eikon_data/fund_holdings_data/aggregated_holdings_600_stocks_test.csv"
+file_path = "/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_Thesis/BA_coding/datasets/eikon_data/fund_holdings_data/aggregated_etf_holdings_600_.csv"
 grouped_df.to_csv(file_path, index=False)
--- python_scripts/refinitiv_eikon_requests/stoxx_europe_600_consituents/constituents_formating.py
+++ python_scripts/refinitiv_eikon_requests/stoxx_europe_600_consituents/constituents_formating.py
@@ -4,20 +4,36 @@ import pandas as pd
 df = pd.read_csv('/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_Thesis/BA_coding/datasets/eikon_data/index_constituents_data/constituents_stoxx_europe_600_merge.csv')
 
 # Create pivot table
-pivot_df = df.pivot_table(index='Date', columns='Constituent RIC', aggfunc='size', fill_value=0)
+pivot_df = df.pivot_table(index='date', columns='stock_RIC', aggfunc='size', fill_value=0)
 
 # Convert to long format
 pivot_df.reset_index(inplace=True)
-long_df = pivot_df.melt(id_vars=['Date'], var_name='stock_RIC', value_name='member')
+long_df = pivot_df.melt(id_vars=['date'], var_name='stock_RIC', value_name='member')
 
 # Adjust the 'member' column to be binary
 long_df['member'] = (long_df['member'] > 0).astype(int)
 
 # Print the resulting DataFrame
+long_df.columns = ["date", "stock_RIC", "index_member"]
 print(long_df)
 
-long_df.columns = ["date", "stock_RIC", "index_member"]
+################### changing dates from end of month to start of next month
+def adjust_date_to_next_month_first(input_df, date_column):
 
-# Save to CSV (optional)
+    # Read the CSV file into a DataFrame
+    df = input_df
+    # Ensure the date column is in datetime format
+    df[date_column] = pd.to_datetime(df[date_column])
+
+    # Adjust the date to the first day of the following month
+    df[date_column] = df[date_column] + pd.DateOffset(days=1)
+    df[date_column] = df[date_column] - pd.to_timedelta(df[date_column].dt.day - 1, unit='d')
+
+    # Write the updated DataFrame back to a new CSV file
+    return df
+    print(f"Dates in column '{date_column}' have been adjusted and saved to {output_file}")
+
+long_df = adjust_date_to_next_month_first(long_df, "date")
+
 file_path = "/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_Thesis/BA_coding/datasets/eikon_data/index_constituents_data/formated_constituents_stoxx_europe_600.csv"
 long_df.to_csv(file_path, index=False)
