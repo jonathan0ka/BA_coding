dcebe98bf7b6d04b18d796d8926d0991bbd533da
jonathan0ka <146357839+jonathan0ka@users.noreply.github.com>
2024-12-03 16:52:06 +0100

    Delete python_scripts directory

--- python_scripts/variable_construction/data_screener.r
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(tidyverse)
-
-df <- read_csv("/Users/jonathanzeh/Library/CloudStorage/OneDrive-Personal/BA_Thesis/BA_coding/datasets/eikon_data/variable_data/monthly_panel_v1.csv")
-
-head(df)
-
-df_test <- df %>% filter(stock_RIC == "NESN.S" & date == as.Date("2023-11-30")) 
-print(df_test[, c("stock_RIC", "date", "stock_value_held", "ETF_ownership", "FUND_ownership")])
